f9ad9ff743789b58c53f95a70b7e0fef993d81f3
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-07-29 01:15:48 +0700

    1.5 Add flashmessage

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 // create a reactive object
-const GStore = reactive({ flashMessage: ''})
+const GStore = reactive({ flashMessage: '' })
 createApp(App).use(store).use(router).provide('GStore', GStore).mount('#app')
