43d25e89afe163104ac0ab9a2e9e2767095ec182
Kongpj <pasakon_paingjai@cmu.ac.th>
2021-07-29 15:05:26 +0700

    4.4

--- src/views/event/Details.vue
+++ src/views/event/Details.vue
@@ -1,10 +1,30 @@
 <template>
   <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
   <p>{{ event.description }}</p>
+   <div id="flash_editMessage" v-if="GStore.flash_editMessage">
+    {{ GStore.flash_editMessage }}
+  </div>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore']
 }
 </script>
+
+<style scoped>
+@keyframes orangefade {
+  from {
+    background: #b99942;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flash_editMessage {
+  animation-name: orangefade;
+  animation-duration: 3s;
+}
+</style>
